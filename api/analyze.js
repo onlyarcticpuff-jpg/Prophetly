3ddcc526b67d55413f92a74ea53f63eea3037def
onlyarcticpuff-jpg <onlyarcticpuff@gmail.com>
2026-04-16 22:37:14 +0200

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key={API_KEY}`,
       {
         method: "POST",
         headers: {
